136558ed71ab1bf7814ea6c46f0aa4fe85971ae7
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-10 19:28:41 +0530

    reverse linked list

--- merge_linkedlist.py
+++ merge_linkedlist.py
@@ -14,5 +14,29 @@ head2.next.next = Node(6)
 head2.next.next.next = Node(8)
 head2.next.next.next.next = Node(10)
 
-dummy = Node(0)
-tail = dummy
+def merge(list1, list2):
+    dummy = Node(0)
+    tail = dummy
+    while list1 is not None and list2 is not None:
+        if list1.data <= list2.data:
+            tail.next = list1
+            list1 = list1.next
+        else:
+            tail.next = list2
+            list2 = list2.next
+        tail = tail.next
+    if list1 is not None:
+        tail.next = list1
+    else:
+        tail.next = list2
+    return dummy.next
+
+def printlist(head):
+    curr = head
+    while curr is not None:
+        print(curr.data, end = " -> ")
+        curr = curr.next
+    print("None")
+
+result = merge(head1, head2)
+printlist(result)
